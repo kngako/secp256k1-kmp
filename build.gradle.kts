50c08d041e50162e61a7771904d818317b0a7521
Kgothatso Ngako <kgothatso@machankura.com>
2026-09-04 20:25:21 +0200

    Minor build updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.util.*
 
 plugins {
     kotlin("multiplatform") version "2.3.10"
-    id("org.jetbrains.dokka") version "2.1.0"
+    id("org.jetbrains.dokka") version "2.2.0"
     `maven-publish`
 }
 
